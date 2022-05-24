3978705a3e636a4afdc4cd7c8783b2b2c95568f4
kziolkowska <aleszczyk.katarzyna@gmail.com>
2022-05-24 11:32:22 +0200

    Add files via upload

---
+++ YoloModelEvaluation/YoloModelEvaluation/Model/YoloModelInputParameters.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoloModelEvaluation.Model
+{
+    public class YoloModelInputParameters
+    {
+        private string modelDataFilePath;
+        private string modelConfigFilePath;
+        private string modelWeightsFilePath;
+
+        public string ModelDataFilePath
+        {
+            get
+            {
+                return modelDataFilePath;
+            }
+            set
+            {
+                if (value.IslDataFileCorrect())
+                {
+                    modelDataFilePath = value; 
+                }
+                else
+                {
+                    modelDataFilePath = string.Empty;
+                }
+            }
+
+        }
+        public string ModelConfigFilePath
+        {
+            get
+            {
+                return modelConfigFilePath;
+            }
+            set
+            {
+                if(value.IsConfigFileCorrect())
+                {
+                    modelConfigFilePath = value;
+                }
+                else
+                {
+                    md
+                }
+            }
+        }
+
+        public string ModelWeightsFilePath
+        {
+            get
+            {
+                return modelWeightsFilePath; 
+                   
+            }
+            set
+            {
+                modelWeightsFilePath = value;
+            }
+        }
+
+
+    }
+}
